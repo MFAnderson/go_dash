6a7f529cd519953865559a1b294f11523d873eeb
Max Anderson <manderso@thoughtworks.com>
2016-01-08 15:21:38 -0800

    added parsing of pipeline_group API

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
+require 'net/http'
+require 'json'
 
 get '/' do
   haml :index 
+end
+
+get '/pipeline_info' do
+  uri = URI("http://#{ENV["HOST"]}/go/api/config/pipeline_groups")
+  request = Net::HTTP::Get.new(uri)
+  request.basic_auth ENV["USER"], ENV["PASS"]
+  response = Net::HTTP.start(uri.hostname, uri.port) {|http|
+    http.request(request)
+  }
+  content_type :json
+  pipeline_groups = JSON.parse response.body
+  display_info = pipeline_groups.map{|group| {name: group["name"], pipelines: group["pipelines"].map{|pipeline| pipeline["name"]}}}
+  display_info.to_json
 end
